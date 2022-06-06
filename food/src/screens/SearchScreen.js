7da28c35920b557d0c19bc42237eb5ea4e3d9e1f
Marina Rozova <marinar578@gmail.com>
2022-06-06 15:39:20 -0400

    Create and style search bar with icon and text input

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import SearchBar from '../components/SearchBar';
 
 const SearchScreen = () => {
-    return <View>
-        <Text>Search Screen</Text>
-    </View>
+    return (
+        <View>
+            <SearchBar />
+            <Text>Search Screen</Text>
+        </View>
+    )
 };
 
 const styles = StyleSheet.create({});
