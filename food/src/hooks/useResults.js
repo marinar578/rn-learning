43d2adb4926b361fe1698723cc9cc68df11a7bfb
Marina Rozova <marinar578@gmail.com>
2022-06-08 15:54:44 -0400

    Extract business logic into re-usable useResults hook

---
+++ food/src/hooks/useResults.js
@@ -0,0 +1,32 @@
+// better name would have been useBusinesses.js, to be more specific
+import { useEffect, useState } from "react";
+import yelp from '../api/yelp'
+
+export default (searchTerm) => {
+    const [results, setResults] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const searchAPI = async (searchTerm) => {
+        try {
+            const response = await yelp.get('/search', {
+                params: {
+                    limit: 50,
+                    term: searchTerm,
+                    location: 'san jose'
+                }
+            });
+            setResults(response.data.businesses);
+            setErrorMessage('');
+        } catch (err) {
+            setErrorMessage("Something went wrong");
+        }
+    }
+
+    // code only runs once when the second argument is an empty array, 
+    // good for running something only upon first render
+    useEffect(() => {
+        searchAPI(searchTerm);
+    }, [])
+
+    return [searchAPI, results, errorMessage];
+}
--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,34 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
-import yelp from '../api/yelp'
+import useResults from '../hooks/useResults';
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
-    const [results, setResults] = useState('');
-    const [errorMessage, setErrorMessage] = useState('');
-
-    const searchAPI = async (searchTerm) => {
-        try {
-            const response = await yelp.get('/search', {
-                params: {
-                    limit: 50,
-                    term: searchTerm,
-                    location: 'san jose'
-                }
-            });
-            setResults(response.data.businesses);
-            setErrorMessage('');
-        } catch (err) {
-            setErrorMessage("Something went wrong");
-        }
-    }
-
-    // code only runs once when the second argument is an empty array, 
-    // good for running something only upon first render
-    useEffect(() => {
-        searchAPI('pasta');
-    }, [])
+    const [searchAPI, results, errorMessage] = useResults('pasta');
 
     return (
         <View>
