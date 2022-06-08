ef9f2d1cba4feea5897ea65c6b554b238a964c73
Marina Rozova <marinar578@gmail.com>
2022-06-08 16:16:21 -0400

    Pass data from ScreenSearch screen to ResultsList component for display

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,11 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import useResults from '../hooks/useResults';
+import ResultsList from '../components/ResultsList';
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
     const [searchAPI, results, errorMessage] = useResults('pasta');
+
+    const filterResultsByPrice = (price) => {
+        return results.filter(result => {
+            return result.price === price;
+        })
+    }
 
     return (
         <View>
@@ -16,11 +23,11 @@ const SearchScreen = () => {
             />
             <Text>Search Screen</Text>
             <Text>{term}</Text>
-            {
-                errorMessage ?
-                    <Text>{errorMessage}</Text> :
-                    <Text>We have found {results.length} results!</Text>
-            }
+            {errorMessage ? <Text>{errorMessage}</Text> : null}
+            <Text>We have found {results.length} results!</Text>
+            <ResultsList results={filterResultsByPrice('$')} title="Cost Effective" />
+            <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier" />
+            <ResultsList results={filterResultsByPrice('$$$')} title="Big Spender" />
         </View>
     )
 };
