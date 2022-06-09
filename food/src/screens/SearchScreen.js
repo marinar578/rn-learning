afc0389f918bb411d0e7cf4131be7c547af9be97
Marina Rozova <marinar578@gmail.com>
2022-06-09 12:31:41 -0400

    Remove View and use an empty JSX wrapper so that the flex 1 fix is unnecessary

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -15,7 +15,7 @@ const SearchScreen = () => {
     }
 
     return (
-        <View styles={{ flex: 1 }}>
+        <>
             <SearchBar
                 term={term}
                 onTermChange={setTerm}
@@ -30,7 +30,7 @@ const SearchScreen = () => {
                 <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier" />
                 <ResultsList results={filterResultsByPrice('$$$')} title="Big Spender" />
             </ScrollView>
-        </View>
+        </>
     )
 };
 
