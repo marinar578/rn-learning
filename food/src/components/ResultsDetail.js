6c7ad36174f1adb3a344548c949d6814c022cfd9
Marina Rozova <marinar578@gmail.com>
2022-06-09 12:11:13 -0400

    Add images, styling, ScrollView for the vertical list, and flex of 1 for most parent view to solve scrolling issue

--- food/src/components/ResultsDetail.js
+++ food/src/components/ResultsDetail.js
@@ -1,10 +1,31 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Image, Text, StyleSheet } from "react-native";
 
 const ResultsDetail = ({ result }) => {
-    return <View><Text>{result.name}</Text></View>
+    return (
+        <View style={styles.container}>
+            <Image style={styles.image} source={{ uri: result.image_url }} />
+            <Text style={styles.name}>{result.name}</Text>
+            <Text>
+                {result.rating} Stars, {result.review_count} Reviews
+            </Text>
+        </View>
+    )
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        marginLeft: 15
+    },
+    image: {
+        width: 250,
+        height: 250,
+        borderRadius: 4,
+        marginBottom: 5
+    },
+    name: {
+        fontWeight: 'bold'
+    }
+});
 
 export default ResultsDetail;
