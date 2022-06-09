e5db4d1b77328e337bbbd9deb2f83045c39175ca
Marina Rozova <marinar578@gmail.com>
2022-06-09 13:14:32 -0400

    Make yelp request for information about specific item

--- food/src/screens/ResultsShowScreen.js
+++ food/src/screens/ResultsShowScreen.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, StyleSheet } from 'react-native';
+import yelp from "../api/yelp";
 
 const ResultsShowScreen = ({ navigation }) => {
     const id = navigation.getParam('id');
+    const [result, setResult] = useState(null);
+
+    console.log(result);
+
+    const getResult = async (id) => {
+        const response = await yelp.get(`/${id}`);
+        setResult(response.data);
+    };
+
+    useEffect(() => {
+        getResult(id);
+    }, []);
+
 
     return (
         <View>
