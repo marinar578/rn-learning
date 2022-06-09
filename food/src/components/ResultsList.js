2ca973770992834a6e08003d96f35685c8dca73e
Marina Rozova <marinar578@gmail.com>
2022-06-09 13:24:02 -0400

    Don't show a section with no results from yelp

--- food/src/components/ResultsList.js
+++ food/src/components/ResultsList.js
@@ -4,6 +4,10 @@ import { withNavigation } from 'react-navigation';
 import ResultsDetail from './ResultsDetail';
 
 const ResultsList = ({ title, results, navigation }) => {
+    if (!results.length) {
+        return null;
+    }
+
     return (
         <View styles={styles.container}>
             <Text style={styles.title}>{title}</Text>
