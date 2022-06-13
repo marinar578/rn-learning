5ffc9c5ed9d6821a55b5a907ab081824ca2a0361
marinar578 <marinar578@gmail.com>
2022-06-13 14:52:08 -0400

    Pass an array of data through context and render with FlatList

--- blog/src/screens/IndexScreen.js
+++ blog/src/screens/IndexScreen.js
@@ -1,14 +1,20 @@
 import React, { useContext } from "react";
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 import BlogContext from "../context/BlogContext";
 
 const IndexScreen = () => {
-    const value = useContext(BlogContext);
-    console.log(value);
+    const blogPosts = useContext(BlogContext);
 
     return (
         <View>
             <Text>Index Screen</Text>
+            <FlatList
+                data={blogPosts}
+                keyExtractor={(blogPost) => blogPost.title}
+                renderItem={({ item }) => {
+                    return <Text>{item.title}</Text>
+                }}
+            />
         </View>
     )
 }
