75120a3639ab4821a194ba2cc2acc7fd95a75f73
marinar578 <marinar578@gmail.com>
2022-06-13 14:21:41 -0400

    Add context file and component for passing data to different screens and nested components.

--- blog/App.js
+++ blog/App.js
@@ -1,18 +1,28 @@
-import { createAppContainer } from "react-navigation";
-import { createStackNavigator } from "react-navigation-stack";
+import React from 'react';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
 import IndexScreen from './src/screens/IndexScreen';
+import { BlogProvider } from './src/context/BlogContext';
 
 const navigator = createStackNavigator(
   {
-
-    Index: IndexScreen
+    Index: IndexScreen,
   },
   {
     initialRouteName: 'Index',
     defaultNavigationOptions: {
-      title: 'Blog'
-    }
+      title: 'Blogs',
+    },
   }
 );
 
-export default createAppContainer(navigator);
+const App = createAppContainer(navigator);
+
+export default () => {
+  return (
+    <BlogProvider>
+      <App />
+    </BlogProvider>
+  );
+};
